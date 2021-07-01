955416edbc179601ee8ed4108b5205b75f772ef0
Mitch Rivet <mitch.rivet@gmail.com>
2021-07-01 12:47:14 -0700

    merged linked lists

---
+++ sorting/merge_k_sorted_linked_lists.js
@@ -0,0 +1,53 @@
+function merge_k_lists(lists) {
+  if (lists.length === 0) {
+    return null;
+  }
+
+  while (lists.length > 1) {
+    const list1 = lists.shift();
+    const list2 = lists.shift();
+    const temp = merge_2_lists(list1, list2);
+    lists.push(temp);
+  }
+  return lists[0];
+}
+
+function merge_2_lists(list1, list2) {
+  if (list1 === null) {
+    return list2;
+  }
+
+  if (list2 === null) {
+    return list1;
+  }
+
+  const dummy = new SinglyLinkedListNode(0);
+  let last = dummy;
+
+  while (list1 !== null && list2 !== null) {
+    if (list1.data > list2.data) {
+      last.next = list2;
+      list2 = list2.next;
+    } else {
+      last.next = list1;
+      list1 = list1.next;
+    }
+    last = last.next;
+  }
+
+  if (list1) {
+    last.next = list1;
+  }
+  if (list2) {
+    last.next = list2;
+  }
+
+  return dummy.next;
+}
+
+const SinglyLinkedListNode = class {
+  constructor(nodeData) {
+    this.data = nodeData;
+    this.next = null;
+  }
+};
--- sorting/two_sum.js
+++ sorting/two_sum.js
@@ -11,3 +11,19 @@ function two_sum(numbers, target) {
   }
   return [-1, -1];
 }
+
+function pair_sum_sorted_array(numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+  while (right > left) {
+    const sum = numbers[left] + numbers[right];
+    if (sum === target) {
+      return [left, right];
+    } else if (sum > target) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return [-1, -1];
+}
