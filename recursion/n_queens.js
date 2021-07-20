afd664f0caf162c716d34ff99cf1e6c1caf71219
Mitch Rivet <mitch.rivet@gmail.com>
2021-07-20 11:29:16 -0700

    n-queens problem

---
+++ recursion/n_queens.js
@@ -0,0 +1,69 @@
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function (n) {
+  const result = [];
+
+  function is_valid(board, row, column) {
+    // so how do we know this will be valid?
+    // check row for queen (we can probably skip this since it's one queen per row)
+    for (let i = column - 1; i >= 0; i--) {
+      if (board[row][i] === "Q") {
+        return false;
+      }
+    }
+
+    // check column
+    for (let j = row - 1; j >= 0; j--) {
+      if (board[j][column] === "Q") {
+        return false;
+      }
+    }
+
+    // check diagonal -> focus on diagonal behind the last placement
+    let a = 1;
+    while (column - a >= 0 && row - a >= 0) {
+      if (board[row - a][column - a] === "Q") {
+        return false;
+      }
+      a++;
+    }
+
+    let b = 1;
+    while (column + b < n && row - b >= 0) {
+      if (board[row - b][column + b] === "Q") {
+        return false;
+      }
+      b++;
+    }
+
+    return true;
+  }
+
+  function backtrack(slate, depth) {
+    if (depth === n) {
+      const final = slate.map((a) => a.join(""));
+      result.push([...final]);
+    } else if (depth > n) {
+      return;
+    } else {
+      for (let i = 0; i < n; i++) {
+        slate[depth][i] = "Q";
+        if (is_valid(slate, depth, i)) {
+          backtrack([...slate], depth + 1);
+        }
+        slate[depth][i] = ".";
+      }
+    }
+  }
+
+  const startSlate = new Array(n).fill(0).map(() => new Array(n).fill("."));
+
+  //pp
+  //const startSlate = new Array(n).fill(Array(n).fill('.'))
+
+  backtrack([...startSlate], 0);
+
+  return result;
+};
--- recursion/sudoku_solver.js
+++ recursion/sudoku_solver.js
@@ -1,45 +1,35 @@
 /**
+ * LC #37 Sudoku Solver
+ *
  * @param {character[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solveSudoku = function (board) {
-  function is_safe(b, row, column) {
-    // check row - so make sure that we don't have any repeats from b[row][0-8], and everything is < 9
-    const rowSet = new Set();
-    for (let i = 0; i < 9; i++) {
-      if (b[row][i] !== ".") {
-        if (rowSet.has(b[row][i])) {
-          return false;
-        }
-        rowSet.add(b[row][i]);
+  function is_safe(row, col, num) {
+    const numStr = num + "";
+    // check row
+    for (let j = 0; j < 9; j++) {
+      numStr;
+      if (board[row][j] === num + "") {
+        return false;
       }
     }
-    const colSet = new Set();
-    // check col - so we fix the column and check each row
-    for (let j = 0; j < 9; j++) {
-      if (b[j][column] !== ".") {
-        if (colSet.has(b[j][column])) {
-          return false;
-        }
-        colSet.add(b[j][column]);
+
+    // check col
+    for (let i = 0; i < 9; i++) {
+      if (board[i][col] === numStr) {
+        return false;
       }
     }
 
     // check subgrid
-    const sub_grid_x = Math.floor(row / 3) * 3;
-    const sub_grid_y = Math.floor(column / 3) * 3;
+    const subgrid_x = Math.floor(row / 3) * 3;
+    const subgrid_y = Math.floor(col / 3) * 3;
 
-    const sub_grid_set = new Set();
-
-    for (let a = sub_grid_x; a < sub_grid_x + 3; a++) {
-      for (let c = sub_grid_y; c < sub_grid_y + 3; c++) {
-        const test = board[a][c];
-        if (test !== ".") {
-          if (sub_grid_set.has(b[a][c])) {
-            return false;
-          }
-
-          sub_grid_set.add(b[a][c]);
+    for (let a = subgrid_x; a < subgrid_x + 3; a++) {
+      for (let b = subgrid_y; b < subgrid_y + 3; b++) {
+        if (board[a][b] === numStr) {
+          return false;
         }
       }
     }
@@ -47,37 +37,37 @@ var solveSudoku = function (board) {
     return true;
   }
 
-  function backtrack(b, row, col) {
-    let unfilled_space = null;
+  function backtrack() {
+    let has_unfilled_square = false;
+    const unfilled_square = { x: null, y: null };
 
-    // loop through whole board and look for first unfilled space
-    for (let i = row; i < 9; i++) {
-      for (let j = col; j < 9; j++) {
-        if (b[i][j] === ".") {
-          unfilled_space = { x: i, y: j };
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] === ".") {
+          has_unfilled_square = true;
+          unfilled_square.x = i;
+          unfilled_square.y = j;
           break;
         }
       }
-      if (!!unfilled_space) {
+      if (has_unfilled_square) {
         break;
       }
     }
 
-    if (!unfilled_space) return true;
-    console.log(unfilled_space);
+    if (!has_unfilled_square) {
+      return true;
+    }
+
+    // with our unfilled square, check each number to see if it's valid
 
     for (let k = 1; k < 10; k++) {
-      if (is_safe(b, unfilled_space.x, unfilled_space.y)) {
-        // then we recurse
-        b[unfilled_space.x][unfilled_space.y] = k + "";
-        if (backtrack(b, unfilled_space.x, unfilled_space.y)) {
+      if (is_safe(unfilled_square.x, unfilled_square.y, k)) {
+        board[unfilled_square.x][unfilled_square.y] = k + "";
+        if (backtrack()) {
           return true;
         } else {
-          // Placing number num to this unfilled cell does not lead to a solution.
-          // So we undo placing it to the board:
-          board.get(row).set(col, 0);
-          // ... and continue with the `for` loop.
-          // That will lead to trying other numbers: backtracking.
+          board[unfilled_square.x][unfilled_square.y] = ".";
         }
       }
     }
@@ -85,7 +75,7 @@ var solveSudoku = function (board) {
     return false;
   }
 
-  backtrack(board, 0, 0);
+  backtrack();
 
   return board;
 };
